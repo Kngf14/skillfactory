224ed93dddf335991cdbc358d4d328e38c1d2841
Kngf14 <120689510+Kngf14@users.noreply.github.com>
2023-03-26 15:54:55 +0300

    Add files via upload

---
+++ NewsPortal/NewsPaper/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import PostList, PostDetail, PostSearch, PostUpdate, PostDelete, post_create
+
+urlpatterns = [
+   path('', PostList.as_view(), name='post_list'),
+   path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+   path('search/', PostSearch.as_view(), name='post_search'),
+   path('create/', post_create, name='post_create'),
+   path('<int:pk>/edit/', PostUpdate.as_view(), name='post_update'),
+   path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),]
