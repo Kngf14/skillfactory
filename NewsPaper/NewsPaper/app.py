314131f8a4cda51ea1edcc8148476488ba298d63
Kngf14 <120689510+Kngf14@users.noreply.github.com>
2023-04-18 16:58:31 +0300

    Add files via upload

---
+++ NewsPaper/NewsPaper/app.py
@@ -0,0 +1,7 @@
+# import redis
+#
+# red = redis.Redis(
+#     host='redis-16700.c299.asia-northeast1-1.gce.cloud.redislabs.com',
+#     port=16700,
+#     password='AdZcwy5qAdohNdSBldeZuRmD5FELgtBJ'
+# )
