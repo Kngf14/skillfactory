84fd39aa929033184b717e8e9951e87a011772b5
Kngf14 <120689510+Kngf14@users.noreply.github.com>
2023-05-23 00:49:33 +0300

    Delete NewsPaper directory

--- NewsPaper/NewsPaper/app.py
+++
@@ -1,7 +0,0 @@
-# import redis
-#
-# red = redis.Redis(
-#     host='redis-16700.c299.asia-northeast1-1.gce.cloud.redislabs.com',
-#     port=16700,
-#     password='AdZcwy5qAdohNdSBldeZuRmD5FELgtBJ'
-# )
